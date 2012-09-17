aa61c63703bec76d47ca6eb2a0ce122414b730a8
Luis Ubaldo <ubaldeitor@ubaldeitor-VirtualBox.(none)>
2012-09-16 21:27:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuautepecol::Application.config.secret_token = '410d7aa6da997e66cb653c5a7971c9048ee8ab98c14cc5b94daba56f9ba49ea61ed10cdf5bf0b6a0c78250bcb78a13c0f921e23bb62dabb6f4ab02fbf9968605'
